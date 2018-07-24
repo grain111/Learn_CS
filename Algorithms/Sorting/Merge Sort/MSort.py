dae063674b9f4abcb47efbf7d2a58a79f144da64
grain111 <grain111@gmail.com>
2018-07-24 23:03:48 +0200

    Implemented merge sort in Python.

---
+++ Algorithms/Sorting/Merge Sort/MSort.py
@@ -0,0 +1,27 @@
+def m_sorter(arr):
+    if len(arr) == 0: return None
+    if len(arr) == 1: return arr
+
+    mid = len(arr) // 2
+    return merge(m_sorter(arr[:mid]), m_sorter(arr[mid:]))
+
+def merge(a, b):
+    # print("Merging {} and {}".format(a, b))
+    temp = []
+    index_a = 0
+    index_b = 0
+
+    for i in range(len(a) + len(b)):
+        if a[index_a] <= b[index_b]:
+            temp.append(a[index_a])
+            index_a += 1
+            if index_a == len(a):
+                temp += b[index_b:]
+                break
+        elif a[index_a] > b[index_b]:
+            temp.append(b[index_b])
+            index_b += 1
+            if index_b == len(b):
+                temp += a[index_a:]
+                break
+    return temp
