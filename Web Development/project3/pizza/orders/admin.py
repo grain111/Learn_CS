4d319b6b9d372a1f789171bf725105f7cabb3a74
grain111 <grain111@gmail.com>
2019-05-13 23:04:30 +0200

    Created models for menu.

--- Web Development/project3/pizza/orders/admin.py
+++ Web Development/project3/pizza/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Group, ExtraIngridient, MenuItem, MenuCombination
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(ExtraIngridient)
+admin.site.register(MenuItem)
+admin.site.register(MenuCombination)
--- Web Development/project3/pizza/orders/models.py
+++ Web Development/project3/pizza/orders/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    """
+    type of food (Pizza, Sub, Salad, ...)
+    """
+
+    type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "{}".format(self.type)
+
+
+class ExtraIngridient(models.Model):
+    """
+    Name
+    """
+
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class MenuItem(models.Model):
+    """
+    Groupq
+    Name
+    AvailbleExtra (Many to Many) with price
+    Price for small
+    Price for large
+    Number of extra ingridients to choose
+    """
+
+    name = models.CharField(max_length=64)
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name="menu_items"
+    )
+    p_small = models.FloatField()
+    p_large = models.FloatField()
+    num_extra = models.IntegerField()
+    extras = models.ManyToManyField(ExtraIngridient, through="MenuCombination", related_name="dishes")
+    # extras = models.ManyToManyField(ExtraIngridient)
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.group)
+
+
+class MenuCombination(models.Model):
+    extra = models.ForeignKey(ExtraIngridient, on_delete=models.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    price = models.FloatField()
